fb36f9006e0d03e0b0517e8f06f997ec59deb5e8
Viktor Trako <viktor.trako@btinternet.com>
2015-09-03 11:24:31 +0100

    modify the main.py script to support out file

--- py/main.py
+++ py/main.py
@@ -2,6 +2,12 @@
     Wrapper around the python lib - pass in a string or a file of user agent strings
     Run:
         # python main.py <inputFormat: string|file> <uaString|filePath> <delimiter: \t|,|json>
+
+    E.g. from file:
+    python main.py file ../test_resources/random_user_agent_strings.txt , ../test_resources/something_nice
+
+    from user agent string:
+    python main.py string "Mozilla/5.0 (Linux; Android 4.2.2; GT-I9195 Build/JDQ39) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.133 Mobile Safari/537.36" ,
 """
 
 __author__ = 'viktor.trako@holidayextras.com (Viktor Trako)'
@@ -11,17 +17,19 @@ import user_agent_wrapper
 # print 'Number of arguments:', len(sys.argv), 'arguments.'
 # print 'Argument List:', str(sys.argv)
 
-if len(sys.argv) == 4:
+if len(sys.argv) >= 4:
     inputType = sys.argv[1]
     inputFormat = sys.argv[2]
     delimiter = sys.argv[3]
-
+    if len(sys.argv) == 5:
+        inFilePath = inputFormat
+        outFilePath = sys.argv[4]
     if inputType == 'string':
         parsedUaString = user_agent_wrapper.parseFromString(inputFormat, delimiter)
         print parsedUaString
-    #     pass it to string parser
+    # pass it to string parser
     if inputType == 'file':
-        user_agent_wrapper.parseFromFile(inputFormat, delimiter)
+        user_agent_wrapper.parseFromFile(inFilePath, outFilePath, delimiter)
     else:
         sys.exit(1)
 else:
